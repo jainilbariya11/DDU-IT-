1dae2710cf69b383fcea52ddd68fd12fa2cada16
Jainil Bariya <jainilbariya820@gmail.com>
2026-09-20 18:04:34 +0530

    Modify array size and enhance code comments
    Increased the array size from 10 to 50 and added comments for clarity.

--- Array/insert.c
+++ Array/insert.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  int i ,N,a[10];
+  int i ,N,a[50];
   int b,pos=0;
 
-  printf("enter number of elements: ");
+  printf("enter number of elements: ");//you want array size
  scanf("%d",&N); 
 printf("---------------------------------\n"); 
      printf("enter the array:\n");
@@ -13,7 +13,7 @@ printf("---------------------------------\n");
     scanf("%d",&a[i]);
   }
   printf("---------------------------------\n"); 
-  printf("your array is:\n"); 
+  printf("your array is:\n"); //print array
    for(i=0;i<N;i++)
   {
     
@@ -27,14 +27,13 @@ printf("Enter the position: ");
  scanf("%d",&pos);
 printf("---------------------------------\n"); 
 
-
-for(i=N;i>pos;i--)
+for(i=N;i>pos;i--)//loop
 {
     a[i]=a[i-1];
 }
   printf("a[%d] = %d\n",pos,b);
  a[pos]=b;    
-printf("---------------------------------\n"); 
+printf("---------------------------------\n"); //decoration
 printf("your new array is:\n");
                            for(i=0;i<=N;i++)
                            {
