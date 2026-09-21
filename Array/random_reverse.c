29b7d66422c7ced3534d9aac99aae661722d5160
Jainil Bariya <jainilbariya820@gmail.com>
2026-09-21 09:06:29 +0530

    Increase array size and enhance comments
    Updated the array size from 10 to 50 and improved comments for clarity.

--- Array/random_reverse.c
+++ Array/random_reverse.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-  int i,j ,N,a[10];
-  int temp,b[10];
+  int i,j ,N,a[50];
+  int temp,b[50];
   int x,y;
 
-  printf("enter number of elements: ");
+  printf("enter number of elements: "); //you want array size according to you.
  scanf("%d",&N); 
 printf("---------------------------------\n"); 
-     printf("enter the array:\n");
+     printf("enter the array:\n");//read arrat
   for(i=0;i<N;i++)
   {
     scanf("%d",&a[i]);
@@ -16,7 +16,7 @@ printf("---------------------------------\n");
   printf("---------------------------------\n"); 
 
 
-  printf("your array is:\n"); 
+  printf("your array is:\n"); //print array
    for(i=0;i<N;i++)
   {
     
@@ -26,14 +26,14 @@ printf("---------------------------------\n");
 
 
 
-    printf("range of index for reverse array like 1 to 3: ");
+    printf("range of index for reverse array like 1 to 3: "); //range 
     scanf("%d %d",&x,&y);
      j=y;
      //i=x;
   //change elements of index of array for reverse like only index 1-3
    if(j<=N)
  {
-  for(i=x,j=y;i<j;i++,j--)
+  for(i=x,j=y;i<j;i++,j--)  // swipe number
   {
  
     
@@ -45,7 +45,7 @@ printf("---------------------------------\n");
   printf("---------------------------------\n"); 
     printf("your array new is:\n");
 
-   for(i=0;i<N;i++)
+   for(i=0;i<N;i++) // new array
   {
     
     printf("%d\n",a[i]);
@@ -53,7 +53,7 @@ printf("---------------------------------\n");
 }
  else
  {
-  printf("\n --> For reverse array your choice index is out of range.\n");
+  printf("\n --> For reverse array your choice index is out of range.\n");//wrong range enter.
  } 
 
     printf("---------------------------------\n"); 
