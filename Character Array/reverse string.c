f84b96cd51b4bb5080d100d4ed4df10666cff0b0
Bhrugu-Patel <bhrugupatel@gmail.com>
2026-09-24 19:36:06 +0530

    Add files via upload
    Program for printing reverse of a string without using another array

---
+++ Character Array/reverse string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+ void main()
+ {
+    char a[100],b;
+    int i,j,k;
+    scanf("%s",a);
+    j=strlen(a);
+    for(i=0;i<j/2;i++)
+    {
+        k=a[i];
+        a[i]=a[j-i-1];
+        a[j-i-1]=k;
+    }
+    printf("%s",a);
+ }
