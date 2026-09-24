d85a8378c15b4ef1de5029ab2d5bf1bfa3b78b0b
Bhrugu-Patel <bhrugupatel@gmail.com>
2026-09-24 19:58:18 +0530

    Add files via upload
    Program for printing reverse of the given word using two arrays

---
+++ Character Array/reverse string another array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+ void main()
+ {
+    char a[100],b[100];
+    int i,j,k;
+    scanf("%s",a);
+    j=strlen(a);
+    k=j-1;
+    for(i=0;i<j;i++)
+    {
+        b[k]=a[i];
+        k--;
+    }
+    printf("The reverse of the given string is %s",b);
+      }
