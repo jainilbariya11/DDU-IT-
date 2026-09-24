c424e90b686061b89a4e9441ebca5b47b7aca549
Jainil Bariya <jainilbariya820@gmail.com>
2026-09-24 20:26:36 +0530

    Improve string reversal program readability
    Refactor string reversal code with comments for clarity.

--- Character Array/reverse string another array.c
+++ Character Array/reverse string another array.c
@@ -2,16 +2,16 @@
 #include<string.h>
  void main()
  {
-    char a[100],b[100];
+    char a[100],b[100]; //size
     int i,j,k;
   printf("Enter the string :\n"):
-    gets(a);
-    j=strlen(a);
+    gets(a); // string enter
+    j=strlen(a); // length
     k=j-1;
-    for(i=0;i<j;i++)
+    for(i=0;i<j;i++) // reverse string using loop.
     {
         b[k]=a[i];
         k--;
     }
-    printf("The reverse of the given string is %s.",b);
+    printf("The reverse of the given string is %s.",b); // also use puts(b);
       }
